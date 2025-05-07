aebebe60d5e5f681ae8edc5cb7c81fd0b35dcc43
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-07 01:53:03 -0500

    fix(user): typo

--- src/main/java/org/sparky/sparkyai/user/infrastructure/UserRepository.java
+++ src/main/java/org/sparky/sparkyai/user/infrastructure/UserRepository.java
@@ -11,7 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-    Optional<User> findByIdAndCompanyId(Long id, Long comapnyId);
+    Optional<User> findByIdAndCompanyId(Long id, Long companyId);
 
     List<User> findByCompanyId(Long id);
 }
