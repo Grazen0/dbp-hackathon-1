a51bfbdaf802a4432ee2cd67e848c1e3a072ef6f
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-07 08:46:00 -0500

    style(test): use spaces

--- src/test/java/org/sparky/sparkyai/SparkyAIApplicationTests.java
+++ src/test/java/org/sparky/sparkyai/SparkyAIApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SparkyAIApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
