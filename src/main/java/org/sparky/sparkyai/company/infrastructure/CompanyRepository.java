83828b1022ce72b1a7cbbfd601ce066e669de39b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-07 01:53:37 -0500

    refactor(company): remove unnecessary findById method

--- src/main/java/org/sparky/sparkyai/company/infrastructure/CompanyRepository.java
+++ src/main/java/org/sparky/sparkyai/company/infrastructure/CompanyRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.sparky.sparkyai.company.domain.Company;
 
-import java.util.Optional;
-
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findById(Long id);
 }
