30aee38c3be843f2bbbc65ca040ebb34a21bedfa
dfigueroaw <dfigueroaw@gmail.com>
2025-05-07 19:29:09 -0500

    feat: add ai controller and ai service

--- src/main/java/org/sparky/sparkyai/ai/application/AiController.java
+++ src/main/java/org/sparky/sparkyai/ai/application/AiController.java
@@ -1,6 +1,9 @@
 package org.sparky.sparkyai.ai.application;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,20 @@ import lombok.RequiredArgsConstructor;
 public class AiController {
 
     private final AiService aiService;
+
+    @PostMapping("/chat")
+    public ResponseEntity<String> chatAi(@RequestBody String message) {
+        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    }
+
+    @PostMapping("/completion")
+    public ResponseEntity<String> ccompletionAi(@RequestBody String message) {
+        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    }
+
+    @PostMapping("/multimodal")
+    public ResponseEntity<String> multimodalAi(@RequestBody String message) {
+        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    }
 
 }
--- src/main/java/org/sparky/sparkyai/ai/domain/AiService.java
+++ src/main/java/org/sparky/sparkyai/ai/domain/AiService.java
@@ -1,8 +1,43 @@
 package org.sparky.sparkyai.ai.domain;
 
+import com.azure.ai.inference.ChatCompletionsClient;
+import com.azure.ai.inference.ChatCompletionsClientBuilder;
+import com.azure.ai.inference.models.ChatCompletions;
+import com.azure.ai.inference.models.ChatCompletionsOptions;
+import com.azure.ai.inference.models.ChatRequestUserMessage;
+import com.azure.core.credential.AzureKeyCredential;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class AiService {
+
+    @Value("${GITHUB_TOKEN}")
+    private String github_token;
+
+    public ResponseEntity<String> createRequest(String model, String requestText) {
+
+        String endpoint = "https://models.github.ai/inference";
+
+        ChatCompletionsClient client = new ChatCompletionsClientBuilder()
+                .credential(new AzureKeyCredential(github_token))
+                .endpoint(endpoint)
+                .buildClient();
+
+        ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions(
+                Collections.singletonList(new ChatRequestUserMessage(requestText))
+        );
+        chatCompletionsOptions.setModel(model);
+
+        ChatCompletions completions = client.complete(chatCompletionsOptions);
+
+        String response = completions.getChoices().getFirst().getMessage().getContent();
+
+        return ResponseEntity.ok(response);
+
+    }
 
 }
