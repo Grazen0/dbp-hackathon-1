b133b89219fe6b51394ab630c1e39ded34fcb718
dfigueroaw <dfigueroaw@gmail.com>
2025-05-07 20:28:03 -0500

    feat: add general model support

--- src/main/java/org/sparky/sparkyai/ai/application/AiController.java
+++ src/main/java/org/sparky/sparkyai/ai/application/AiController.java
@@ -2,10 +2,7 @@ package org.sparky.sparkyai.ai.application;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.sparky.sparkyai.ai.domain.AiService;
 
@@ -20,18 +17,18 @@ public class AiController {
     private final AiService aiService;
 
     @PostMapping("/chat")
-    public ResponseEntity<String> chatAi(@RequestBody String message) {
-        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    public ResponseEntity<String> chatAi(@RequestParam(defaultValue = "meta/Llama-4-Scout-17B-16E-Instruct") String model, @RequestBody String message) {
+        return aiService.createRequest(model, message);
     }
 
     @PostMapping("/completion")
-    public ResponseEntity<String> ccompletionAi(@RequestBody String message) {
-        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    public ResponseEntity<String> completionAi(@RequestParam(defaultValue = "meta/Llama-4-Scout-17B-16E-Instruct") String model, @RequestBody String message) {
+        return aiService.createRequest(model, message);
     }
 
     @PostMapping("/multimodal")
-    public ResponseEntity<String> multimodalAi(@RequestBody String message) {
-        return aiService.createRequest("meta/Llama-4-Scout-17B-16E-Instruct", message);
+    public ResponseEntity<String> multimodalAi(@RequestParam(defaultValue = "meta/Llama-4-Scout-17B-16E-Instruct") String model, @RequestBody String message) {
+        return aiService.createRequest(model, message);
     }
 
 }
